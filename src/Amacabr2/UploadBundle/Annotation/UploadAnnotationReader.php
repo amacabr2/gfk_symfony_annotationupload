3c485326951a9ed44c829ee6dc170a5575b94a25
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-29 11:57:40 +0200

    Recovery of the properties of the note " UploadableField "

--- src/Amacabr2/UploadBundle/Annotation/UploadAnnotationReader.php
+++ src/Amacabr2/UploadBundle/Annotation/UploadAnnotationReader.php
@@ -22,7 +22,22 @@ class UploadAnnotationReader {
 
     public  function isUploadable($entity): bool {
         $reflection = new \ReflectionClass(get_class($entity));
-        return $this->reader->getClassAnnotation($reflection, Uploadable::class);
+        return $this->reader->getClassAnnotation($reflection, Uploadable::class) !== null;
+    }
+
+    public function getUploadbleFields($entity): array {
+        $reflection = new \ReflectionClass(get_class($entity));
+        if (!$this->isUploadable($entity)) {
+            return [];
+        }
+        $properties = [];
+        foreach ($reflection->getProperties() as $property) {
+            $annotation = $this->reader->getPropertyAnnotation($property, UploadableField::class);
+            if ($annotation !== null) {
+                $properties[$property->getName()] = $annotation;
+            }
+        }
+        return $properties;
     }
 
 }
